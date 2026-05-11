74e96cb0699ee1fe072eff95d03e7a4c62d34cac
djmonkeyuk <charliembbanks@gmail.com>
2026-05-11 20:41:54 +0100

    Data driven mirror utils for less hardcoded values in py file

--- src/addons/no_mans_sky_base_builder/utils/mirror_utils.py
+++ src/addons/no_mans_sky_base_builder/utils/mirror_utils.py
@@ -1,59 +1,74 @@
+import json
 import math
-import mathutils
+from pathlib import Path
+
 from mathutils import Matrix, Euler, Vector
 
-#parts that need 180 degress rotation across local Y axis
-y_180_local_correction_parts = {"S_SAIL0","BASE_CAVE3"}
+MIRROR_CORRECTIONS_PATH = (
+    Path(__file__).resolve().parents[1] / "resources" / "mirror_corrections.json"
+)
 
 
-#This function mirrors matrix world across x axis
-def mirror_matrix_world(object_id,old_matrix_world, across_x = True):
-    
-    #extract location,rotation and scale values from matrix world
+def load_mirror_corrections():
+    with open(MIRROR_CORRECTIONS_PATH, "r", encoding="utf-8") as stream:
+        return json.load(stream)
+
+
+MIRROR_CORRECTIONS = load_mirror_corrections()
+LOCAL_Y_180_ROTATION_PARTS = set(MIRROR_CORRECTIONS["local_y_180_rotation_parts"])
+MIRROR_Z_180_IDENTIFIERS = set(MIRROR_CORRECTIONS["mirror_z_180_identifiers"])
+POSITION_OFFSETS = {
+    part: Vector(offset)
+    for part, offset in MIRROR_CORRECTIONS["position_offsets"].items()
+}
+
+
+# This function mirrors matrix world across x axis
+def mirror_matrix_world(object_id, old_matrix_world, across_x=True):
+
+    # extract location,rotation and scale values from matrix world
     location, rotation_quaternion, scale = old_matrix_world.decompose()
-    
-    #mirror location  if across x is selected
-    new_location = -location.x if across_x else location.x 
+
+    # mirror location  if across x is selected
+    new_location = -location.x if across_x else location.x
     position_values = (new_location, location.y, location.z)
     position_matrix = Matrix.Translation(Vector(position_values))
-    
-    #mirror rotation across x axis
-    current_euler = rotation_quaternion.to_euler('XYZ')
+
+    # mirror rotation across x axis
+    current_euler = rotation_quaternion.to_euler("XYZ")
     rotation_values = (current_euler.x, -current_euler.y, -current_euler.z)
-    rotation_euler = Euler(rotation_values, 'XYZ')
+    rotation_euler = Euler(rotation_values, "XYZ")
     rotation_matrix = rotation_euler.to_matrix().to_4x4()
-    
-    #creating scale matrix
+
+    # creating scale matrix
     scale_matrix = Matrix.Scale(scale.x, 4)
-    
-    #multiplying all the matrix
+
+    # multiplying all the matrix
     matrix_world = position_matrix @ rotation_matrix @ scale_matrix
-    
-    #correct anomalies in mirroring
-    matrix_world = mirror_correction(object_id,matrix_world)
-    
+
+    # correct anomalies in mirroring
+    matrix_world = mirror_correction(object_id, matrix_world)
+
     return matrix_world
 
-# This function provides additional corrctions after mirroring object
-def mirror_correction(object_id,matrix_world):
-    
+
+# This function provides additional corrections after mirroring object
+def mirror_correction(object_id, matrix_world):
     # All triangular floor tiles
-    if "TRIFLOOR" in object_id:
-        angle = math.pi # 180 degrees
-        z_rot_180 = Matrix.Rotation(angle, 4, 'Z')
+    if any(identifier in object_id for identifier in MIRROR_Z_180_IDENTIFIERS):
+        angle = math.pi  # 180 degrees
+        z_rot_180 = Matrix.Rotation(angle, 4, "Z")
         return matrix_world @ z_rot_180
-    
-    # Storage panel is off centered by 0.05444555 units of measurement
-    elif object_id == "STORAGEPANEL":
-        storage_panel_offset = 0.05444555
-        local_offset = mathutils.Vector((storage_panel_offset, 0.0, 0.0))
-        translation_matrix = mathutils.Matrix.Translation(local_offset)
+
+    # Apply per-part position offset corrections
+    if object_id in POSITION_OFFSETS:
+        translation_matrix = Matrix.Translation(POSITION_OFFSETS[object_id])
         return matrix_world @ translation_matrix
-    
-    # Parts that need rotation by 180 on y axis locally go here
-    elif object_id in y_180_local_correction_parts:
-        angle = math.pi # 180 degrees
-        y_rot_180 = Matrix.Rotation(angle, 4, 'Y')
+
+    # Parts that need rotation by 180 on local Y axis
+    if object_id in LOCAL_Y_180_ROTATION_PARTS:
+        angle = math.pi  # 180 degrees
+        y_rot_180 = Matrix.Rotation(angle, 4, "Y")
         return matrix_world @ y_rot_180
-        
+
     return matrix_world
