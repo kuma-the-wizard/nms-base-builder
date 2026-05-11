2526e08d71d1edc8ce0b5a79bcf8a9c61689b9c1
djmonkeyuk <charliembbanks@gmail.com>
2026-05-11 20:44:42 +0100

    revert auto format

--- src/addons/no_mans_sky_base_builder/utils/mirror_utils.py
+++ src/addons/no_mans_sky_base_builder/utils/mirror_utils.py
@@ -23,51 +23,51 @@ POSITION_OFFSETS = {
 }
 
 
-# This function mirrors matrix world across x axis
+#This function mirrors matrix world across x axis
 def mirror_matrix_world(object_id, old_matrix_world, across_x=True):
 
-    # extract location,rotation and scale values from matrix world
+    #extract location,rotation and scale values from matrix world
     location, rotation_quaternion, scale = old_matrix_world.decompose()
 
-    # mirror location  if across x is selected
+    #mirror location  if across x is selected
     new_location = -location.x if across_x else location.x
     position_values = (new_location, location.y, location.z)
     position_matrix = Matrix.Translation(Vector(position_values))
 
-    # mirror rotation across x axis
+    #mirror rotation across x axis
     current_euler = rotation_quaternion.to_euler("XYZ")
     rotation_values = (current_euler.x, -current_euler.y, -current_euler.z)
     rotation_euler = Euler(rotation_values, "XYZ")
     rotation_matrix = rotation_euler.to_matrix().to_4x4()
 
-    # creating scale matrix
+    #creating scale matrix
     scale_matrix = Matrix.Scale(scale.x, 4)
 
-    # multiplying all the matrix
+    #multiplying all the matrix
     matrix_world = position_matrix @ rotation_matrix @ scale_matrix
 
-    # correct anomalies in mirroring
+    #correct anomalies in mirroring
     matrix_world = mirror_correction(object_id, matrix_world)
 
     return matrix_world
 
 
-# This function provides additional corrections after mirroring object
+#This function provides additional corrections after mirroring object
 def mirror_correction(object_id, matrix_world):
-    # All triangular floor tiles
+    #All triangular floor tiles
     if any(identifier in object_id for identifier in MIRROR_Z_180_IDENTIFIERS):
-        angle = math.pi  # 180 degrees
+        angle = math.pi  #180 degrees
         z_rot_180 = Matrix.Rotation(angle, 4, "Z")
         return matrix_world @ z_rot_180
 
-    # Apply per-part position offset corrections
+    #Apply per-part position offset corrections
     if object_id in POSITION_OFFSETS:
         translation_matrix = Matrix.Translation(POSITION_OFFSETS[object_id])
         return matrix_world @ translation_matrix
 
-    # Parts that need rotation by 180 on local Y axis
+    #Parts that need rotation by 180 on local Y axis
     if object_id in LOCAL_Y_180_ROTATION_PARTS:
-        angle = math.pi  # 180 degrees
+        angle = math.pi  #180 degrees
         y_rot_180 = Matrix.Rotation(angle, 4, "Y")
         return matrix_world @ y_rot_180
 
